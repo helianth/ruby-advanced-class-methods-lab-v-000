ac0373d4f3d789236b2f6a6ef64d9719b63944f4
Lamia Haider <lamia.haider@gmail.com>
2020-03-30 06:54:26 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -52,9 +52,10 @@ class Song
     song
   end
 
-  def self.create_from_filename
-    self.new_from_filename
+  def self.create_from_filename(filename)
+    self.new_from_filename(filename)
     song.save
+    
   end
 
 
